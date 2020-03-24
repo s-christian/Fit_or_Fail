3cfd9e867efdaed7e4c0e717e313b215ec4d3f16
s-christian <61216427+s-christian@users.noreply.github.com>
2020-03-23 23:54:34 -0400

    Added wins index to User model to count total number of wins

--- project/models/User.js
+++ project/models/User.js
@@ -33,6 +33,10 @@ const UserSchema = new Schema({
         type: Number,
         default: 0
     },
+    wins: {
+        type: Number,
+        default: 0
+    },
     team: {
         type: String,
         default: ''
