c382cc9f6583d6f6ff4f887a4d6e3334e0a89835
s-christian <61216427+s-christian@users.noreply.github.com>
2020-03-24 22:37:29 -0400

    Added registration handling for existing usernames or emails

--- project/routes/api/register.js
+++ project/routes/api/register.js
@@ -15,8 +15,9 @@ const User = require('../../models/User');
 router.post('/', (req, res) => {
     let { username, email, password, confirm_password, account_type } = req.body;
 
-    let valid_account_types = ['user', 'parent', 'gov', 'admin'];
-    let errors = {};
+    let valid_account_types = ['user', 'gov', 'admin'];
+    let validation_errors = {};
+    let account_errors = {};
 
     // Convert empty fields to an empty string so we can use validator functions
     // Essentially, if the field doesn't exist, we just create it as an empty string
@@ -29,95 +30,105 @@ router.post('/', (req, res) => {
     // Validation
     // Username checks
     if (Validator.isEmpty(username)) {
-        errors.username = "Username field is required";
+        validation_errors.username = "Username field is required";
     } else if (!Validator.isAlphanumeric(username, 'en-US')) {
-        errors.username = "Username cannot contain symbols";
+        validation_errors.username = "Username cannot contain symbols";
     }
 
     // Email checks
     if (Validator.isEmpty(email)) {
-        errors.email = "Email field is required";
+        validation_errors.email = "Email field is required";
     } else if (!Validator.isEmail(email)) {
-        errors.email = "Email is invalid";
+        validation_errors.email = "Email is invalid";
     }
 
     // Password checks
     if (!Validator.isLength(password, { min: 6, max: 32 })) {
-        errors.password = "Password must be 6-32 characters";
+        validation_errors.password = "Password must be 6-32 characters";
     }
     if (Validator.isEmpty(password)) {
-        errors.password = "Password field is required";
+        validation_errors.password = "Password field is required";
     }
     if (!Validator.equals(password, confirm_password)) {
-        errors.confirm_password = "Passwords do not match";
+        validation_errors.confirm_password = "Passwords do not match";
     }
     if (Validator.isEmpty(confirm_password)) {
-        errors.confirm_password = "Confirm password field is required";
+        validation_errors.confirm_password = "Confirm password field is required";
     }
 
     // Account Type checks
     if (account_type !== "" && !valid_account_types.includes(account_type)) {
-        errors.account_type = "Invalid account type";
+        validation_errors.account_type = "Invalid account type";
     }
 
-    // If we have any errors, return
-    if (!isEmpty(errors)) {
-        return res.status(400).json({ msg: 'Provided fields are incorrect due to the following errors', errors });
+    // If we have any validation_errors, return
+    if (!isEmpty(validation_errors)) {
+        return res.status(400).json({ msg: 'Provided fields are incorrect due to the following errors', validation_errors });
     }
 
     // Check for existing user if register input is valid
-    User.findOne({ email })
+    User.findOne({ username })
         .then(user => {
-            if (user) return res.status(400).json({ msg: 'User already exists' });
-            
-            let newUser;
-            if (account_type == "") {
-                newUser = new User({
-                    username,
-                    email,
-                    password,
-                });
-            } else {
-                newUser = new User({
-                    username,
-                    email,
-                    password,
-                    account_type
-                });
-            }
+            if (user) account_errors.username = "Username already taken";
+            User.findOne({ email })
+                .then(user => {
+                    if (user) account_errors.email = "Email already registered";
 
-            // Create salt and hash
-            bcrypt.genSalt(10, (err, salt) => {
-                bcrypt.hash(newUser.password, salt, (err, hash) => {
-                    if (err) throw err;
-                    newUser.password = hash;
-                    newUser.save()  // add new user to database
-                        .then(user => {
-                            // Issue token
-                            jwt.sign(
-                                { id: user.id },
-                                process.env.JWT_SECRET,
-                                { expiresIn: 3600 },  // token/session expires in one hour
-                                (err, token) => {
-                                    if (err) throw err;
-                                    res.json({
-                                        token,
-                                        user: {
-                                            id: user.id,
-                                            username: user.username,
-                                            email: user.email,
-                                            profile_picture_url: user.profile_picture_url,
-                                            points: user.points,
-                                            team: user.team,
-                                            account_type: user.account_type
-                                        }
-                                    });
-                                }
-                            );
+                    // If we have any account_errors, return
+                    if (!isEmpty(account_errors)) {
+                        return res.status(400).json({ msg: 'Cannot register this username or email', account_errors });
+                    }
+
+                    // Go on to create new user and register in database
+                    let newUser;
+                    if (account_type == "") {
+                        newUser = new User({
+                            username,
+                            email,
+                            password,
                         });
+                    } else {
+                        newUser = new User({
+                            username,
+                            email,
+                            password,
+                            account_type
+                        });
+                    }
+                 
+                    // Create salt and hash
+                    bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            if (err) throw err;
+                            newUser.password = hash;
+                            newUser.save()  // add new user to database
+                                .then(user => {
+                                    // Issue token
+                                    jwt.sign(
+                                        { id: user.id },
+                                        process.env.JWT_SECRET,
+                                        { expiresIn: 3600 },  // token/session expires in one hour
+                                        (err, token) => {
+                                            if (err) throw err;
+                                            res.json({
+                                                token,
+                                                user: {
+                                                    id: user.id,
+                                                    username: user.username,
+                                                    email: user.email,
+                                                    profile_picture_url: user.profile_picture_url,
+                                                    points: user.points,
+                                                    team: user.team,
+                                                    account_type: user.account_type
+                                                }
+                                            });
+                                        }
+                                    );
+                                });
+                        });
+                    });
                 });
-            });
         });
 });
-
+ 
 module.exports = router;
