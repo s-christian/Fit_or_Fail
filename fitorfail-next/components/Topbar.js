31009aaf09fd037fae026aa0b7815ac0663a5d9a
s-christian <61216427+s-christian@users.noreply.github.com>
2020-04-02 03:47:29 -0400

    Placed temporary logo in navbar

--- fitorfail-next/components/Topbar.js
+++ fitorfail-next/components/Topbar.js
@@ -3,40 +3,57 @@ import Link from "next/link";
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
 import styled, { css } from "styled-components";
 
+const StyledNavbar = styled(Navbar)`
+	background-color: hsl(119, 0%, 95%);
+`;
+
+const TopbarLink = styled.a`
+	height: 100%;
+	display: flex;
+	align-items: center;
+	text-decoration: none;
+	font-weight: 700;
+	color: #212121;
+	&:hover {
+		text-decoration: none;
+		color: hsl(189, 67%, 49%);
+	}
+
+	/* This function allows you to pass this component keywords to provide different CSS styling. */
+	/* In this case, any TopbarLink with "primary" results in a custom style. */
+	${(props) =>
+		props.primary &&
+		css`
+			color: hsl(189, 78%, 39%);
+			font-weight: 900;
+			font-size: 1.25rem;
+		`}
+`;
+
 const Topbar = () => {
 	const [isOpen, setIsOpen] = useState(false);
 	const toggle = () => setIsOpen(!isOpen);
 
-	const StyledNavbar = styled(Navbar)`
-		background-color: hsl(119, 0%, 95%);
-	`;
-
-	const TopbarLink = styled.a`
-		text-decoration: none;
-		font-weight: 700;
-		color: #212121;
-		&:hover {
-			text-decoration: none;
-			color: hsl(189, 67%, 49%);
-		}
-
-		/* This function allows you to pass this component keywords to provide different CSS styling. */
-		/* In this case, any TopbarLink with "primary" results in a custom style. */
-		${(props) =>
-			props.primary &&
-			css`
-				color: hsl(189, 78%, 39%);
-				font-weight: 900;
-				font-size: 1.25rem;
-			`}
-	`;
-
 	return (
 		// The second "light" is the color scheme for the hamburger icon
+		// TODO: Make "Play!" in the Topbar a button so it visually pops
 		<StyledNavbar light expand="md">
 			<NavbarBrand>
 				<Link href="/" passHref>
-					<TopbarLink primary>Fit or Fail</TopbarLink>
+					<TopbarLink primary>
+						{/* Maybe just a temporary image, but it looks nice */}
+						<img
+							src="/assets/images/fitorfail_logo_small.jpg"
+							alt="Fit or Fail logo"
+							style={{
+								height: "30px",
+								borderRadius: "50%",
+								marginRight: "0.5rem",
+								border: "1px solid black"
+							}}
+						/>
+						Fit or Fail
+					</TopbarLink>
 				</Link>
 			</NavbarBrand>
 			<NavbarToggler onClick={toggle} />
--- fitorfail-next/pages/index.js
+++ fitorfail-next/pages/index.js
@@ -3,59 +3,80 @@ import { Container, Jumbotron } from "reactstrap";
 import styled, { css } from "styled-components";
 import Link from "next/link";
 
+const StyledJumbo = styled(Jumbotron)`
+	text-align: center;
+	background-color: hsl(189, 78%, 39%);
+	margin-bottom: 2px;
+
+	${(props) =>
+		props.secondary &&
+		css`
+			background-color: red;
+		`}
+`;
+
+const Title = styled.h1`
+	font-size: 5rem;
+	color: white;
+	font-weight: 900;
+	text-shadow: 2px 2px gray;
+
+	${(props) =>
+		props.secondary &&
+		css`
+			font-size: 3rem;
+			text-shadow: none;
+		`}
+`;
+
+const Subtitle = styled.p`
+	font-size: 1.25rem;
+	font-weight: 600;
+	color: white;
+	font-style: italic;
+
+	${(props) =>
+		props.secondary &&
+		css`
+			font-size: 1rem;
+		`}
+`;
+
+const ButtonContainer = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-basis: 400px;
+`;
+
+const StyledButton = styled.button`
+	width: 11rem;
+	background-color: hsl(0, 75%, 60%);
+	border-radius: 3px;
+	border: 2px solid hsl(188, 54%, 82%);
+	color: white;
+	font-weight: 900;
+	margin: 0.5rem 1rem;
+	padding: 0.5rem 0;
+	&:hover {
+		background-color: hsl(0, 91%, 83%);
+		transition: 500ms;
+	}
+
+	${(props) =>
+		props.secondary &&
+		css`
+			background-color: white;
+			color: black;
+			font-weight: 600;
+			&:hover {
+				background-color: hsla(108, 52%, 56%);
+			}
+		`}
+`;
+
 const Index = () => {
-	const StyledJumbo = styled(Jumbotron)`
-		text-align: center;
-		background-color: hsl(189, 78%, 39%);
-	`;
-
-	const Title = styled.h1`
-		font-size: 5rem;
-		color: white;
-		font-weight: 900;
-		text-shadow: 2px 2px gray;
-	`;
-
-	const Subtitle = styled.p`
-		font-size: 1.25rem;
-		font-weight: 600;
-		color: white;
-		font-style: italic;
-	`;
-
-	const ButtonContainer = styled.div`
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		flex-basis: 400px;
-	`;
-
-	const StyledButton = styled.button`
-		width: 11rem;
-		background-color: hsl(0, 75%, 60%);
-		border-radius: 3px;
-		border: 2px solid hsl(188, 54%, 82%);
-		color: white;
-		font-weight: 900;
-		margin: 0.5rem 1rem;
-		padding: 0.5rem 0;
-		&:hover {
-			background-color: hsl(0, 91%, 83%);
-			transition: 500ms;
-		}
-
-		${(props) =>
-			props.secondary &&
-			css`
-				background-color: white;
-				color: black;
-				font-weight: 600;
-				&:hover {
-					background-color: hsla(108, 52%, 56%);
-				}
-			`}
-	`;
-
+	// Trying to add our fitorfail_logo.png behind the main text, but can't figure out how to
 	return (
 		<Layout title="Welcome">
 			<StyledJumbo fluid>
@@ -72,10 +93,18 @@ const Index = () => {
 					</ButtonContainer>
 				</Container>
 			</StyledJumbo>
-
-			<Link href="/testing">
-				<a>Link to testing page</a>
-			</Link>
+			<StyledJumbo fluid secondary>
+				<Container fluid="lg">
+					<Title secondary>Leaderboard</Title>
+					<Subtitle secondary>There's gonna be a cool leaderboard, woohoo</Subtitle>
+					<Link href="/testing">
+						<a>Leaderboard</a>
+					</Link>
+					<Link href="/testing">
+						<a>Link to testing page</a>
+					</Link>
+				</Container>
+			</StyledJumbo>
 		</Layout>
 	);
 };
