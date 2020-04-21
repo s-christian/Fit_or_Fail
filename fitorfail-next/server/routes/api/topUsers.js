8e163afbb2dde690d5bd63b0a7d16424d31fd81e
s-christian <61216427+s-christian@users.noreply.github.com>
2020-04-21 19:52:25 -0400

    API for leaderboard and retrieving user statistics

---
+++ fitorfail-next/server/routes/api/topUsers.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+
+// Item model
+const User = require("../../models/User");
+
+router.get("/", (req, res) => {
+	// Get the top 10 players, sorted by points value
+	User.aggregate(
+		[
+			{
+				$project: {
+					username: 1,
+					team: 1,
+					register_date: 1,
+					profile_picture_url: 1,
+					points: 1,
+					totalAnswers: 1,
+					correctAnswers: 1,
+					percentageCorrect: { $divide: ["$correctAnswers", "$totalAnswers"] },
+					wins: 1
+				}
+			},
+			{ $sort: { points: -1 } },
+			{ $limit: 10 } // send the top 10 Users, sorted in descending order by their points value
+		],
+		(err, results) => {
+			if (err) return res.json({ err });
+			return res.send(results);
+		}
+	);
+});
+
+module.exports = router;
---
+++ fitorfail-next/server/routes/api/userStatistics.js
@@ -0,0 +1,101 @@
+/* Resources:
+ * https://docs.mongodb.com/manual/tutorial/aggregation-with-user-preference-data/
+ * https://docs.mongodb.com/manual/reference/operator/aggregation/group/#pipe._S_group
+ * https://stackoverflow.com/questions/22873614/mongodb-aggregation-sum-values-of-a-key-in-a-collection
+ * https://stackoverflow.com/questions/21803290/get-a-count-of-total-documents-with-mongodb-when-using-limit/21803405
+ * https://docs.mongodb.com/manual/reference/operator/aggregation/facet/
+ */
+
+const express = require("express");
+const router = express.Router();
+
+const authRole = require("../../middleware/authRole");
+const authGov = authRole("gov");
+
+// Item model
+const User = require("../../models/User");
+
+router.get("/scores", authGov, (req, res) => {
+	User.aggregate(
+		[
+			{
+				$group: {
+					_id: null, // group everything together
+					pointTotal: { $sum: "$points" },
+					answerTotal: { $sum: "$totalAnswers" },
+					correctAnswerTotal: { $sum: "$correctAnswers" },
+					winTotal: { $sum: "$wins" }
+				}
+			}
+		],
+		(err, results) => {
+			if (err) return res.json({ err });
+			return res.send(results[0]);
+		}
+	);
+});
+
+router.get("/registrations", authGov, (req, res) => {
+	// Return number of Users registered by year and month
+
+	User.aggregate(
+		[
+			{
+				// "Processes multiple aggregation pipelines within a single stage on the same set of input documents."
+				$facet: {
+					total_registrations: [{ $count: "total_registrations" }],
+					registrations_by_date: [
+						{
+							$group: {
+								// _id is the required first first used to group documents
+								// _id: <expression>,
+								_id: {
+									month: { $month: "$register_date" },
+									year: { $year: "$register_date" }
+								},
+								// <field1>: { <accumulator1> : <expression1> },
+								// ...
+								accounts_registered: { $sum: 1 } // increment by 1 (from 0) for every document that matches this group (based off of the User's register_date)
+							}
+						},
+						{ $sort: { "_id.month": -1, "_id.year": -1 } }
+					]
+				}
+			}
+		],
+		(err, results) => {
+			if (err) return res.json({ err });
+			// Maybe not the most elegant solution, but it allows me to get the total registration count and group the registrations within one aggregation
+			new_results = {
+				total_registrations: results[0].total_registrations[0].total_registrations,
+				registrations_by_date: results[0].registrations_by_date
+			};
+			return res.json(new_results);
+		}
+	);
+
+	// User.aggregate(
+	// 	[
+	// 		{
+	// 			$group: {
+	// 				// _id is the required first first used to group documents
+	// 				// _id: <expression>,
+	// 				_id: {
+	// 					month: { $month: "$register_date" },
+	// 					year: { $year: "$register_date" }
+	// 				},
+	// 				// <field1>: { <accumulator1> : <expression1> },
+	// 				// ...
+	// 				accounts_registered: { $sum: 1 } // increment by 1 (from 0) for every document that matches this group (based off of the User's register_date)
+	// 			}
+	// 		},
+	// 		{ $sort: { "_id.month": -1, "_id.year": -1 } }
+	// 	],
+	// 	(err, results) => {
+	// 		if (err) return res.json({ err });
+	// 		return res.send(results);
+	// 	}
+	// );
+});
+
+module.exports = router;
