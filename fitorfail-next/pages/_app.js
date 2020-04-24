3ff221d567881204204c6cae7e361c99dad12330
s-christian <61216427+s-christian@users.noreply.github.com>
2020-04-24 02:00:50 -0400

    lang attribute now on Html tag in custom _document. Also, 200th commit!

--- fitorfail-next/pages/_app.js
+++ fitorfail-next/pages/_app.js
@@ -58,6 +58,7 @@ const MyApp = ({ Component, pageProps }) => {
 		<>
 			<Head>
 				<meta charSet="utf-8" />
+				<meta name="description" content="Fit or Fail!" />
 				<meta
 					name="viewport"
 					content="width=device-width, initial-scale=1, shrink-to-fit=no"
